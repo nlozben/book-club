7dbb7964b9b6626d6bc8c8d1618829898429177d
Steven Gov <steven.gov@housecallpro.com>
2023-02-23 13:27:47 -0800

    Created `ProfilePic` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import ProfilePic from './components/ProfilePic'
 import SwipeButton from './components/SwipeButton'
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
+      <ProfilePic />
       <SwipeButton title="left swipe"/>
       <SwipeButton title="right swipe"/>
     </div>
--- src/components/ProfilePic.tsx
+++ src/components/ProfilePic.tsx
@@ -1,1 +1,10 @@
-export {};
+import React from "react"
+
+const ProfilePic = () => {
+	const logo = require('../images/plumber1.jpg'); 
+  return (
+		<img src={logo} />
+  )
+}
+
+export default ProfilePic;
