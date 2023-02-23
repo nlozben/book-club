d0a22dd21cf78c52ee54a8d1995cf999acb6df1b
Steven Gov <steven.gov@housecallpro.com>
2023-02-23 13:34:01 -0800

    Used `Grid` to layout homepage

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import ProfilePic from './components/ProfilePic'
 import SwipeButton from './components/SwipeButton'
+import { Grid } from "@mui/material";
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <ProfilePic />
-      <SwipeButton title="left swipe"/>
-      <SwipeButton title="right swipe"/>
+      <Grid container direction="column" spacing={2}>
+        <Grid item>
+          <ProfilePic />
+        </Grid>
+        <Grid item>
+          <SwipeButton title="left swipe"/>
+          <SwipeButton title="right swipe"/>
+        </Grid>
+      </Grid>
     </div>
   );
 }
