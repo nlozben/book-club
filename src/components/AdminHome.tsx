ff240c6079570b703d39cf619883f38a5626420e
Steven Gov <steven.gov@housecallpro.com>
2023-03-02 13:02:32 -0800

    Used `Window.postMessage()` to send data from parent to child window

--- src/components/AdminHome.tsx
+++ src/components/AdminHome.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
 const AdminHome = () => {
+	let popup: Window | null;
+
+	const openPopup = () => {
+		popup = window.open('http://example.localhost:3000', 'popup', 'height=300px, width=500px');
+	};
+
+	const sendMessage = () => {
+		const message = {
+			fullName: 'Steven is Cool',
+			phoneNumber: '(626) 774-4130',
+		}
+
+    popup!.postMessage(message, 'http://example.localhost:3000')
+    popup!.focus();
+	}
+
 	return (
 		<div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
 			<h1 style={{ color: 'black' }}>Admin App home</h1>
+			<br />
+			<button onClick={openPopup}>Open Dialer</button>
+			<button onClick={sendMessage}>Send message</button>
 		</div>
 	)
 }
--- src/components/ExampleHome.tsx
+++ src/components/ExampleHome.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ExampleHome = () => {
+
+	const [data, setData] = useState({
+		fullName: '',
+		phoneNumber: ''
+	});
+
+	window.addEventListener("message", (event)=>{
+		if (event.origin !== 'http://admin.localhost:3000') {
+			return null;
+		}
+		setData(event.data);
+	});
+
 	return (
 		<div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-			<h1 style={{ color: 'black' }}>Example App home</h1>
+			<h1 style={{ color: 'black' }}>Example App home (Dialer)</h1>
+			<body>
+				<input type="text" id="fullName" name="fullName" value={data.fullName} /> <br />
+				<input type="text" id="phoneNumber" name="phoneNumber" value={data.phoneNumber} />
+			</body>
 		</div>
 	)
 }
